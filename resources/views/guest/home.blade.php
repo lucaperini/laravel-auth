357faa609755f9d7714eee3c0a107379bdb7d96e
lucaperini <luca.perini@gmail.com>
2022-05-23 08:11:36 +0200

    completed

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center align-items-center my-5">
+        <h1 class="text-center fw-bold p-5">Benvenut* in Boolpress!</h1>
+        <p class="text-center my-0">Per accedere al nostro archivio Entra o Registrati. Grazie.</p>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware("auth")
+->namespace('Admin')
+->prefix('admin')
+->name('admin.')
+->group (function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+});
+
+Route::get('/{any}', 'Guest\HomeController@index')->where('any', '.*');
